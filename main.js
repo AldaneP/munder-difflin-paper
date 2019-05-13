64120c56c222a0afa89edc1435346d66cedb805e
Aldane Purrier <aldanepur@gmail.com>
2019-05-13 16:57:49 -0400

    I'm proud of all this code I typed so far

---
+++ main.js
@@ -0,0 +1,23 @@
+// let dunder = document.getElementsByTagName("section")
+//
+// window.addEventListener('scroll', function() {
+//   if (window.scrollTo(0,161)) {
+//     dunder[0].classList.remove('hidden')
+//     dunder[0].classList.add('show')
+//   }
+//
+//   if (window.scrollTo(0,968)) {
+//     dunder[1].classList.remove('hidden')
+//     dunder[1].classList.add('show')
+//   }
+//
+//   if (window.scrollTo(0,1796)) {
+//     dunder[2].classList.remove('')
+//     dunder[2].classList.add('')
+//   }
+//
+//   if (window.scrollTo(0,2554)) {
+//     dunder[3].classList.remove('hidden')
+//     dunder[3].classList.add('show')
+//   }
+// })
